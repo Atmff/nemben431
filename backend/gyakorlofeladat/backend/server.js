33bf68b8b3e435c7e6ba40a424c8727ccef16c71
Atmff <150684144+Atmff@users.noreply.github.com>
2025-01-24 13:18:04 +0100

    vegre jo

--- backend/gyakorlofeladat/backend/server.js
+++ backend/gyakorlofeladat/backend/server.js
@@ -2,20 +2,30 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mysql = require("mysql");
-const bodyParser = require('body-parser');
 const bodyParser = require("body-parser");
-app.use(bodyParser.json());
-
-app.use(cors());
-
-const db = mysql.createConnection({
-    user: "root",
+app .use(bodyParser.json());
+ 
+const db = mysql.createConnection(
+    {      
+     user:"root",
     host:"127.0.0.1",
-    port: 3306,
     password:"",
     database:"kozutak",
-});
-
+    }
+);
+ 
 app.get("/", (req, res) => {
-    res.send("Express is running");
+    res.send("Fut a backend!");
 })
+ 
+app.get("/regiok",(req, res) => {
+    const sql = "SELECT * FROM `regiok`";
+    db.query(sql, (err, result) => {
+        if(err) return res.json(err);
+        return res.json(result)
+    })
+})
+ 
+app.listen(3001, () => {
+    console.log("Server is runningg on port 3001");
+});
