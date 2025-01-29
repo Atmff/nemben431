509a5919de4041474c978cf30756f56fbedf164a
Atmff <150684144+Atmff@users.noreply.github.com>
2025-01-29 13:12:03 +0100

    pikpakjo

--- backend/gyakorlofeladat/backend/server.js
+++ backend/gyakorlofeladat/backend/server.js
@@ -9,6 +9,7 @@ const db = mysql.createConnection(
     {      
      user:"root",
     host:"127.0.0.1",
+    port:"3307",
     password:"",
     database:"kozutak",
     }
@@ -18,6 +19,7 @@ app.get("/", (req, res) => {
     res.send("Fut a backend!");
 })
  
+
  app.get("/regiok",(req, res) => {
     const sql = "SELECT * FROM `regiok`";
     db.query(sql, (err, result) => {
@@ -26,6 +28,17 @@ app.get("/", (req, res) => {
     })
 })
  
+app.post("/ujregio", (req, res) => {
+    const { Rid, regionev, regio_tipusa } = req.body; 
+    const sql = "INSERT INTO `regiok` (`Rid`, `regionev`, `regio_tipusa`) VALUES (?, ?, ?)";
+    const VALUES = ['11', 'Budapest', 'Főváros']; 
+
+    db.query(sql, VALUES, (err, result) => {
+        if (err) return res.json(err); 
+        return res.json({ message: "Sikeres beszúrás", result }); 
+    });
+});
+
 app.listen(3001, () => {
     console.log("Server is runningg on port 3001");
 }); 
