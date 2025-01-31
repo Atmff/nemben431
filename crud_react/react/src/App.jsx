38d474489c838267a6e84e81b1acc4e73985dc13
nemben <nemben431@hengersor.hu>
2025-01-31 13:21:04 +0100

    senkinemtudjamitortentora

--- crud_react/react/src/App.jsx
+++ crud_react/react/src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './Home'
+import Regiok from './Regiok'
+
 
 function App() {
+  return (
+    <div>
+      <BrowserRouter>   
+         <Routes>
+        <Route path="/" elements={<Home/>}/>
+        <Route path="/regiok" element={<Regiok/>}/>
+      </Routes>
+      
+      </BrowserRouter>
 
+    </div>
+  )
 }
 
 export default App
