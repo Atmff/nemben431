93b5e35950ac21c5355c6b9f3ac4e74dde1312ad
nemben <nemben431@hengersor.hu>
2025-03-19 12:09:17 +0100

    otthoin

---
+++ otthoni/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+app .use(bodyParser.json());
+ 
+const db = mysql.createConnection(
+    {      
+     user:"root",
+    host:"127.0.0.1",
+    password:"",
+    database:"kozutak",
+    }
+);
+
+app.get("/", (req, res) => {
+    res.send("Fut a backend!");
+})
+
+app.listen(3001, () => {
+    console.log("Server is runningg on port 3001");
+}); 
